3aaab888a207ac875c46933481b96aed6c5319aa
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-03-17 20:41:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
+    origin: 'http://localhost:3000'
 }));
 
 // Rate limiting
