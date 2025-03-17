09227dfd83af4a4adecab59e497607c720ddd5a9
Het Shah <59816505+shahet1124@users.noreply.github.com>
2025-03-17 23:44:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use(cors({
-    origin: 'https://tss-frontend-sand.vercel.app/',
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
